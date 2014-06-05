7eeeb0d51047a6b750a3e78e3169129d7755fb0c
Tavis Rudd <tavis@damnsimple.com>
2014-06-05 00:34:19 -0700

    flesh out tests

--- tests.bat
+++ tests.bat
@@ -1,6 +1,25 @@
 #!/usr/bin/env bats
 ## tests with https://github.com/sstephenson/bats
 
+die() {
+    echo "$@" >/dev/stderr
+    exit 1
+}
+
+export DOCKHACK_SKIP_UID_CHECK=1
+
+################################################################################
+## mocked docker command
+#which docker &>/dev/null || die "ERROR: docker must be installed to run tests"
+source $BATS_TEST_DIRNAME/mock_docker # for test fixture variables
+export mock_docker="$BATS_TEST_DIRNAME/mock_docker"
+docker() {
+    "$mock_docker" "$@"
+}
+export -f docker
+################################################################################
+
+
 @test "running with no args prints usage & exit code 2." {
   run ./dockhack
   [ "$status" -eq 2 ]
@@ -21,4 +40,18 @@
   run ./dockhack lib
   [ "$status" -eq 0 ]
   [[ "$output" = "" ]]
+}
+
+@test "'get_id last' == 'get_id l' == 'id last'" {
+  local -a commands=('get_id last' 'get_id l' 'id last')
+  for com in "${commands[@]}"; do
+      eval "run ./dockhack $com"
+      echo ">> $com : $output"
+      [[ "$status" -eq 0 ]]
+      [[ "$output" = "$TEST_ID" ]] || die "bad out"
+  done
+}
+
+@test "dummy test" {
+    true
 }
